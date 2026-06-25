6a703f2b6f0013e661bdf9e2006fa0f2cdbc1c1a
DeVine-byte <Divine4529@gmail.com>
2026-06-25 11:46:53 +0100

    Add EntityNode class for AST representation

---
+++ compiler/compiler-parser/ast/EntityNode.java
@@ -0,0 +1,23 @@
+package com.devinebyte.compiler.parser.ast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityNode extends AstNode {
+
+    private String name;
+
+    private final List<PropertyNode> properties = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PropertyNode> getProperties() {
+        return properties;
+    }
+}
