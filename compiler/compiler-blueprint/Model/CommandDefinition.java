cc3387913497695733c8a92d5eb9b37c3f07ea4a
DeVine-byte <Divine4529@gmail.com>
2026-06-25 12:32:16 +0100

    Add CommandDefinition class for command management

---
+++ compiler/compiler-blueprint/Model/CommandDefinition.java
@@ -0,0 +1,15 @@
+package com.devinebyte.compiler.blueprint.model;
+
+public class CommandDefinition {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
