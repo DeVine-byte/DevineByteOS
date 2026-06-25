b3e4cb0903186b99a1be9f3912a2da698e042196
DeVine-byte <Divine4529@gmail.com>
2026-06-25 12:28:50 +0100

    Add EntityDefinition class for model representation

---
+++ compiler/compiler-blueprint/Model/EntityDefinition.java
@@ -0,0 +1,24 @@
+package com.devinebyte.compiler.blueprint.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityDefinition {
+
+    private String name;
+
+    private final List<PropertyDefinition> properties = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PropertyDefinition> getProperties() {
+        return properties;
+    }
+
+}
