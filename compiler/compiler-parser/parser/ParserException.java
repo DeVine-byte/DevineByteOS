43bb1e25cd1486eeb92414c4e501966880b030f3
DeVine-byte <Divine4529@gmail.com>
2026-06-25 11:42:34 +0100

    Add ParserException class for parser errors

---
+++ compiler/compiler-parser/parser/ParserException.java
@@ -0,0 +1,9 @@
+package com.devinebyte.compiler.parser.parser;
+
+public class ParserException extends RuntimeException {
+
+    public ParserException(String message) {
+        super(message);
+    }
+
+}
