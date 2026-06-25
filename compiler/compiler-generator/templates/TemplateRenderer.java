6b3f24a05a664161ae02ff9a200ee5e8c05adb07
DeVine-byte <Divine4529@gmail.com>
2026-06-25 15:28:44 +0100

    Add TemplateRenderer class for rendering templates

---
+++ compiler/compiler-generator/templates/TemplateRenderer.java
@@ -0,0 +1,11 @@
+package com.devinebyte.compiler.generator.templates;
+
+public class TemplateRenderer {
+
+    public String render(String template, Object model) {
+
+        return template;
+
+    }
+
+}
