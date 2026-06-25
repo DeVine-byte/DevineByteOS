d9e7657d65f5b86f74c27eac5acf995fc4513696
DeVine-byte <Divine4529@gmail.com>
2026-06-25 12:27:07 +0100

    Create BlueprintModel class for entity and workflow management
    Added BlueprintModel class to manage entity and workflow definitions.

---
+++ compiler/compiler-blueprint/Model/BlueprintModel.java
@@ -0,0 +1,20 @@
+package com.devinebyte.compiler.blueprint.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlueprintModel {
+
+    private final List<EntityDefinition> entities = new ArrayList<>();
+
+    private final List<WorkflowDefinition> workflows = new ArrayList<>();
+
+    public List<EntityDefinition> getEntities() {
+        return entities;
+    }
+
+    public List<WorkflowDefinition> getWorkflows() {
+        return workflows;
+    }
+
+}
