6444cbc41fd767ad2799f9057578b981f0c300b0
DeVine-byte <Divine4529@gmail.com>
2026-06-25 15:09:16 +0100

    Create ServiceGenerator.java

---
+++ compiler/compiler-generator/java/ServiceGenerator.java
@@ -0,0 +1,13 @@
+package com.devinebyte.compiler.generator.java;
+
+import com.devinebyte.compiler.blueprint.model.EntityDefinition;
+
+public class ServiceGenerator {
+
+    public String generate(EntityDefinition entity) {
+
+        return "";
+
+    }
+
+}
