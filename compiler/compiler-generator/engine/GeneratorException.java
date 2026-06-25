41b928ba3027ae159f0e07505ec67e549dc71175
DeVine-byte <Divine4529@gmail.com>
2026-06-25 15:01:08 +0100

    Add GeneratorException class for error handling

---
+++ compiler/compiler-generator/engine/GeneratorException.java
@@ -0,0 +1,9 @@
+package com.devinebyte.compiler.generator.engine;
+
+public class GeneratorException extends RuntimeException {
+
+    public GeneratorException(String message) {
+        super(message);
+    }
+
+}
