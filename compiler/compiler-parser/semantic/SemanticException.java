f6dbb11de07451fcb61e3cd714dfffa7872c300e
DeVine-byte <Divine4529@gmail.com>
2026-06-25 11:52:19 +0100

    Add SemanticException class for error handling

---
+++ compiler/compiler-parser/semantic/SemanticException.java
@@ -0,0 +1,9 @@
+package com.devinebyte.compiler.parser.semantic;
+
+public class SemanticException extends RuntimeException {
+
+    public SemanticException(String message) {
+        super(message);
+    }
+
+}
