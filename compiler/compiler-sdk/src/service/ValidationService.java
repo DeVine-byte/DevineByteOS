f7f019e7008ecd396b56841366d45de94e22c69c
DeVine-byte <Divine4529@gmail.com>
2026-06-25 21:41:56 +0100

    Add ValidationService interface for request validation

---
+++ compiler/compiler-sdk/src/service/ValidationService.java
@@ -0,0 +1,10 @@
+package org.devinebyte.sdk.service;
+
+import org.devinebyte.sdk.Request;
+import org.devinebyte.sdk.Result;
+
+public interface ValidationService {
+
+    Result validate(Request request);
+
+}
