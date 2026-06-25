1e552355b5326e1a257167a3695fe1f68698547d
DeVine-byte <Divine4529@gmail.com>
2026-06-25 15:22:55 +0100

    Add RestGenerator class for generating REST APIs

---
+++ compiler/compiler-generator/api/RestGenerator.java
@@ -0,0 +1,13 @@
+package com.devinebyte.compiler.generator.api;
+
+import com.devinebyte.compiler.blueprint.model.BlueprintModel;
+
+public class RestGenerator {
+
+    public String generate(BlueprintModel blueprint) {
+
+        return "";
+
+    }
+
+}
