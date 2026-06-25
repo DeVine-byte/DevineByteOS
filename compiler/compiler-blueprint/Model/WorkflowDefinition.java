ee5b5fc9273d2d466073bbca039251370ad425dc
DeVine-byte <Divine4529@gmail.com>
2026-06-25 12:31:12 +0100

    Add WorkflowDefinition class with name property

---
+++ compiler/compiler-blueprint/Model/WorkflowDefinition.java
@@ -0,0 +1,15 @@
+package com.devinebyte.compiler.blueprint.model;
+
+public class WorkflowDefinition {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
