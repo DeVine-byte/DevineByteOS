a4b822d12b6d48be76a6f6402293327cc81444f9
DeVine-byte <Divine4529@gmail.com>
2026-06-25 21:32:33 +0100

    Add Builder class for project configuration

---
+++ compiler/compiler-sdk/src/Builder.java
@@ -0,0 +1,48 @@
+package org.devinebyte.sdk;
+
+import java.nio.file.Path;
+
+public class Builder {
+
+    private Path projectRoot;
+    private Path sourceDirectory;
+    private Path outputDirectory;
+    private boolean incremental;
+    private boolean optimize;
+
+    public Builder projectRoot(Path root) {
+        this.projectRoot = root;
+        return this;
+    }
+
+    public Builder sourceDirectory(Path source) {
+        this.sourceDirectory = source;
+        return this;
+    }
+
+    public Builder outputDirectory(Path output) {
+        this.outputDirectory = output;
+        return this;
+    }
+
+    public Builder incremental(boolean value) {
+        this.incremental = value;
+        return this;
+    }
+
+    public Builder optimize(boolean value) {
+        this.optimize = value;
+        return this;
+    }
+
+    public Session build() {
+        return new Session(
+                projectRoot,
+                sourceDirectory,
+                outputDirectory,
+                incremental,
+                optimize
+        );
+    }
+
+}
