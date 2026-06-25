85f84ad9f8906ca29c083ff0a3744d49448b74d5
DeVine-byte <Divine4529@gmail.com>
2026-06-25 12:34:51 +0100

    Add ApiDefinition class with basePath property

---
+++ compiler/compiler-blueprint/Model/ApiDefinition.java
@@ -0,0 +1,15 @@
+package com.devinebyte.compiler.blueprint.model;
+
+public class ApiDefinition {
+
+    private String basePath;
+
+    public String getBasePath() {
+        return basePath;
+    }
+
+    public void setBasePath(String basePath) {
+        this.basePath = basePath;
+    }
+
+}
