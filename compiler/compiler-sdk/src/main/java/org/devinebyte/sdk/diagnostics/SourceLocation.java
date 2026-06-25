05f389e965c19dcc310615d592512593393fd8c3
DeVine-byte <Divine4529@gmail.com>
2026-06-25 23:40:33 +0100

    Add SourceLocation class for file diagnostics

---
+++ compiler/compiler-sdk/src/main/java/org/devinebyte/sdk/diagnostics/SourceLocation.java
@@ -0,0 +1,29 @@
+package org.devinebyte.sdk.diagnostics;
+
+import java.nio.file.Path;
+
+public final class SourceLocation {
+
+    private final Path file;
+    private final int line;
+    private final int column;
+
+    public SourceLocation(Path file, int line, int column) {
+        this.file = file;
+        this.line = line;
+        this.column = column;
+    }
+
+    public Path getFile() {
+        return file;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+}
