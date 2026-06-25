e6b60837ec27f93a14a471e63375c00351264f60
DeVine-byte <Divine4529@gmail.com>
2026-06-25 12:33:15 +0100

    Add EventDefinition class for event modeling

---
+++ compiler/compiler-blueprint/Model/EventDefinition.java
@@ -0,0 +1,15 @@
+package com.devinebyte.compiler.blueprint.model;
+
+public class EventDefinition {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
