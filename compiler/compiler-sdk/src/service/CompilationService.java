0fb56cd644eee72883d4cc9b5a39f5d4e19ef2c2
DeVine-byte <Divine4529@gmail.com>
2026-06-25 21:45:08 +0100

    Create CompilationService interface for compilation

---
+++ compiler/compiler-sdk/src/service/CompilationService.java
@@ -0,0 +1,10 @@
+package org.devinebyte.sdk.service;
+
+import org.devinebyte.sdk.Request;
+import org.devinebyte.sdk.Result;
+
+public interface CompilationService {
+
+    Result compile(Request request);
+
+}
