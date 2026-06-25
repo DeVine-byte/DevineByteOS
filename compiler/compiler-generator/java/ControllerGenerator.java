83fe2d952fe28db98a911f3142968bfee947ab28
DeVine-byte <Divine4529@gmail.com>
2026-06-25 15:14:24 +0100

    Add ControllerGenerator class for entity generation

---
+++ compiler/compiler-generator/java/ControllerGenerator.java
@@ -0,0 +1,13 @@
+package com.devinebyte.compiler.generator.java;
+
+import com.devinebyte.compiler.blueprint.model.EntityDefinition;
+
+public class ControllerGenerator {
+
+    public String generate(EntityDefinition entity) {
+
+        return "";
+
+    }
+
+}
