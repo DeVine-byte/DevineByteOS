b546910dd47a6fa42586760f0d09d02dd9b4b977
DeVine-byte <Divine4529@gmail.com>
2026-06-25 21:40:24 +0100

    Add ParsingService interface for request parsing

---
+++ compiler/compiler-sdk/src/service/ParsingService.java
@@ -0,0 +1,10 @@
+package org.devinebyte.sdk.service;
+
+import org.devinebyte.sdk.Request;
+import org.devinebyte.sdk.Result;
+
+public interface ParsingService {
+
+    Result parse(Request request);
+
+}
