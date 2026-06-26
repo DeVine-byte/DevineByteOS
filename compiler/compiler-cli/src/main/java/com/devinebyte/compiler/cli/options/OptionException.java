d10baf0ec70e27053daa3c119f4483409e90ac69
DeVine-byte <Divine4529@gmail.com>
2026-06-26 18:44:51 +0100

    Add OptionException class for handling options errors

---
+++ compiler/compiler-cli/src/main/java/com/devinebyte/compiler/cli/options/OptionException.java
@@ -0,0 +1,8 @@
+package com.devinebyte.compiler.cli.options;
+
+public class OptionException extends RuntimeException {
+
+    public OptionException(String message) {
+        super(message);
+    }
+}
