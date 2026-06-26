29a70433f54caddf1420048a759419f9938eaea5
DeVine-byte <Divine4529@gmail.com>
2026-06-26 18:27:18 +0100

    Implement HelpCommand to display usage instructions

---
+++ compiler/compiler-cli/src/main/java/com/devinebyte/compiler/cli/commands/HelpCommand.java
@@ -0,0 +1,28 @@
+package com.devinebyte.compiler.cli.commands;
+
+import com.devinebyte.compiler.cli.util.ExitCodes;
+
+public final class HelpCommand implements Command {
+
+    @Override
+    public int execute() {
+
+        System.out.println("""
+                DevineByte Compiler
+
+                Usage:
+                  db <command>
+
+                Commands:
+                  compile    Compile source files
+                  build      Build a project
+                  parse      Parse source files
+                  validate   Validate source files
+                  version    Display compiler version
+                  help       Display this help
+                """);
+
+        return ExitCodes.SUCCESS;
+    }
+
+}
