d0768a9e65d06046661798292efac3f1fce6c0f7
DeVine-byte <Divine4529@gmail.com>
2026-06-26 16:33:21 +0100

    Add ResultMapper class for mapping results

---
+++ compiler/compiler-sdk/src/main/java/org/devinebyte/sdk/internal/ResultMapper.java
@@ -0,0 +1,14 @@
+package org.devinebyte.sdk.internal;
+
+import org.devinebyte.sdk.Result;
+
+public final class ResultMapper {
+
+    private ResultMapper() {
+    }
+
+    public static Result success() {
+        return Result.success();
+    }
+
+}
