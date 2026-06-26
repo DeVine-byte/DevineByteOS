228383d0087168457241d171a921b43bac99f699
DeVine-byte <Divine4529@gmail.com>
2026-06-26 19:15:09 +0100

    Implement AnsiConsole for colored console output

---
+++ compiler/compiler-cli/src/main/java/com/devinebyte/compiler/cli/console/AnsiConsole.java
@@ -0,0 +1,44 @@
+package com.devinebyte.compiler.cli.console;
+
+public class AnsiConsole implements Console {
+
+    private final boolean colorsEnabled;
+
+    public AnsiConsole() {
+        this.colorsEnabled = System.console() != null;
+    }
+
+    @Override
+    public void info(String message) {
+        printColored(message, ConsoleColor.BLUE);
+    }
+
+    @Override
+    public void success(String message) {
+        printColored(message, ConsoleColor.GREEN);
+    }
+
+    @Override
+    public void warning(String message) {
+        printColored(message, ConsoleColor.YELLOW);
+    }
+
+    @Override
+    public void error(String message) {
+        printColored(message, ConsoleColor.RED);
+    }
+
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+
+    private void printColored(String message, ConsoleColor color) {
+
+        if (colorsEnabled) {
+            System.out.println(color.ansi() + message + ConsoleColor.RESET.ansi());
+        } else {
+            System.out.println(message);
+        }
+    }
+}
