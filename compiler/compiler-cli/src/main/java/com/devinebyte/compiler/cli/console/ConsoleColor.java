c8e929702a729fe3cc4efefa9af3148c90f1d85b
DeVine-byte <Divine4529@gmail.com>
2026-06-26 19:08:26 +0100

    Add ConsoleColor enum for ANSI color codes

---
+++ compiler/compiler-cli/src/main/java/com/devinebyte/compiler/cli/console/ConsoleColor.java
@@ -0,0 +1,28 @@
+package com.devinebyte.compiler.cli.console;
+
+public enum ConsoleColor {
+
+    RESET("\u001B[0m"),
+
+    RED("\u001B[31m"),
+
+    GREEN("\u001B[32m"),
+
+    YELLOW("\u001B[33m"),
+
+    BLUE("\u001B[34m"),
+
+    CYAN("\u001B[36m"),
+
+    WHITE("\u001B[37m");
+
+    private final String ansi;
+
+    ConsoleColor(String ansi) {
+        this.ansi = ansi;
+    }
+
+    public String ansi() {
+        return ansi;
+    }
+}
