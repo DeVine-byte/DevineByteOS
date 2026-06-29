7e5ad5bc26cbdaf1bc91d16b5a917239e215ec00
DeVine-byte <Divine4529@gmail.com>
2026-06-29 17:39:33 +0100

    Add Java plugin and update dependencies

--- compiler/compiler-core/build.gradle.kts
+++ compiler/compiler-core/build.gradle.kts
@@ -1,11 +1,22 @@
+plugins {
+    `java-library`
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
-
     implementation(project(":compiler-diagnostics"))
-
-    testImplementation(project(":compiler-testing"))
+    implementation(project(":compiler-sdk"))
 
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
-
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.27.3")
+}
 
+tasks.test {
+    useJUnitPlatform()
 }
