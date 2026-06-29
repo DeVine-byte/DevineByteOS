b883587e6f0a0b98ec4a0990cd6715f43cc29e15
DeVine-byte <Divine4529@gmail.com>
2026-06-29 17:54:15 +0100

    Create ArtifactGenerationServiceTest.java

---
+++ compiler/compiler-sdk/src/test/java/org/devinebyte/compiler/sdk/ArtifactGenerationServiceTest.java
@@ -0,0 +1,19 @@
+package com.devinebyte.compiler.sdk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ArtifactGenerationServiceTest {
+
+    @Test
+    void shouldProvideArtifactGenerationService() {
+
+        CompilerSDK sdk =
+                CompilerSDK.builder().build();
+
+        assertNotNull(sdk.artifactGenerationService());
+
+    }
+
+}
