1ac7b2232b38debc506c41f70e5ceed9c6e65783
DeVine-byte <Divine4529@gmail.com>
2026-06-29 15:34:44 +0100

    Add unit test for CompilerConfiguration class

---
+++ compiler/compiler-core/src/test/java/org/devinebyte/compiler/core/CompilerConfigurationTest.java
@@ -0,0 +1,28 @@
+package org.devinebyte.compiler.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class CompilerConfigurationTest {
+
+    @Test
+    void shouldStoreConfigurationValues() {
+
+        CompilerConfiguration configuration =
+                new CompilerConfiguration(
+                        "build",
+                        false,
+                        true
+                );
+
+        assertEquals("build", configuration.outputDirectory());
+
+        assertFalse(configuration.strictMode());
+
+        assertEquals(true, configuration.incremental());
+
+    }
+
+}
