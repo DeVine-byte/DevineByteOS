78eaefc6c18edeead0e702dfca540f6267dc9883
DeVine-byte <Divine4529@gmail.com>
2026-06-29 17:52:24 +0100

    Create ValidationServiceTest.java

---
+++ compiler/compiler-sdk/src/test/java/org/devinebyte/compiler/sdk/ValidationServiceTest.java
@@ -0,0 +1,19 @@
+package org.devinebyte.compiler.sdk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ValidationServiceTest {
+
+    @Test
+    void shouldProvideValidationService() {
+
+        CompilerSDK sdk =
+                CompilerSDK.builder().build();
+
+        assertNotNull(sdk.validationService());
+
+    }
+
+}
