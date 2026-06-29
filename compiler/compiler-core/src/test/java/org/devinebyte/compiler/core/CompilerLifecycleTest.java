8c6285784bcd66a4f2e01fb599409efd87e06bf7
DeVine-byte <Divine4529@gmail.com>
2026-06-29 15:38:33 +0100

    Add test for multiple starts of CompilerEngine

---
+++ compiler/compiler-core/src/test/java/org/devinebyte/compiler/core/CompilerLifecycleTest.java
@@ -0,0 +1,20 @@
+package org.devinebyte.compiler.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class CompilerLifecycleTest {
+
+    @Test
+    void shouldAllowMultipleStarts() {
+
+        CompilerEngine engine = new CompilerEngine();
+
+        assertDoesNotThrow(engine::start);
+
+        assertDoesNotThrow(engine::start);
+
+    }
+
+}
