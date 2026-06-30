e2ea813e176dd350c09c382ef8051bd1fe3db4bf
DeVine-byte <Divine4529@gmail.com>
2026-06-30 10:16:09 +0100

    Add tests for OptionParser functionality

---
+++ compiler/compiler-cli/src/test/java/org/devinebyte/compiler/cli/options/OptionParserTest.java
@@ -0,0 +1,46 @@
+package org.devinebyte.compiler.cli.options;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OptionParserTest {
+
+    @Test
+    void shouldParseInputOutputAndFlags() {
+
+        OptionParser parser = new OptionParser();
+
+        CliOptions options = parser.parse(new String[]{
+                "--input", "src",
+                "--output", "build",
+                "--incremental",
+                "--strict",
+                "--optimize",
+                "--verbose"
+        });
+
+        assertEquals(Path.of("src"), options.getInput());
+        assertEquals(Path.of("build"), options.getOutput());
+        assertTrue(options.isIncremental());
+        assertTrue(options.isStrict());
+        assertTrue(options.isOptimize());
+        assertTrue(options.isVerbose());
+
+    }
+
+    @Test
+    void shouldThrowExceptionForUnknownOption() {
+
+        OptionParser parser = new OptionParser();
+
+        assertThrows(
+                OptionException.class,
+                () -> parser.parse(new String[]{"--unknown"})
+        );
+
+    }
+
+}
