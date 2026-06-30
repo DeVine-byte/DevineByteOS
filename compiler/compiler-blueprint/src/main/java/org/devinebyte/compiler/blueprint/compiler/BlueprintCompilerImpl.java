d1968a54ef7968c22f0e4c6257d900a9d0ab4014
DeVine-byte <Divine4529@gmail.com>
2026-06-30 13:59:46 +0100

    Add overloaded compile method in BlueprintCompilerImpl
    Added overloaded compile method for BlueprintModel.

--- compiler/compiler-blueprint/src/main/java/org/devinebyte/compiler/blueprint/compiler/BlueprintCompilerImpl.java
+++ compiler/compiler-blueprint/src/main/java/org/devinebyte/compiler/blueprint/compiler/BlueprintCompilerImpl.java
@@ -1,16 +1,18 @@
 package org.devinebyte.compiler.blueprint.compiler;
 
+import org.devinebyte.compiler.blueprint.model.BlueprintModel;
 import org.devinebyte.compiler.parser.ast.ProgramNode;
 
 public class BlueprintCompilerImpl implements BlueprintCompiler {
 
     @Override
     public BlueprintCompilationResult compile(ProgramNode program) {
-
         BlueprintModel model = new BlueprintModel();
-
         return new BlueprintCompilationResult(model, true);
-
     }
 
+    public BlueprintModel compile(BlueprintModel model) {
+        // TODO: Implement logic
+        return model;
+    }
 }
