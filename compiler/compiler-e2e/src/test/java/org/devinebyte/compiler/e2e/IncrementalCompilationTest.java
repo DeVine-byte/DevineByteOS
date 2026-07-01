51d895bd66e9999ec1d3eeffc9a23c2b79a6d3be
DeVine-byte <Divine4529@gmail.com>
2026-07-01 18:29:36 +0100

    Add incremental compilation test case

---
+++ compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/IncrementalCompilationTest.java
@@ -0,0 +1,14 @@
+package org.devinebyte.compiler.e2e;
+
+import org.devinebyte.compiler.testing.assertions.CompilationAssertions;
+import org.junit.jupiter.api.Test;
+
+class IncrementalCompilationTest extends CompilerE2ETestSupport {
+
+    @Test
+    void shouldSupportIncrementalCompilation() {
+        compile("crm"); // first pass
+        CompilerResult second = compileIncremental("crm");
+        CompilationAssertions.succeeded(second.success());
+    }
+}
