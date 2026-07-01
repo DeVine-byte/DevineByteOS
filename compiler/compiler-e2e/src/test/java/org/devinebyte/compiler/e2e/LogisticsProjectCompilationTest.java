4fa4cbded3a7df3ec5778350eacd23784f9c6ce5
DeVine-byte <Divine4529@gmail.com>
2026-07-01 18:27:56 +0100

    Add end-to-end test for Logistics project compilation

---
+++ compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/LogisticsProjectCompilationTest.java
@@ -0,0 +1,13 @@
+package org.devinebyte.compiler.e2e;
+
+import org.devinebyte.compiler.testing.assertions.CompilationAssertions;
+import org.junit.jupiter.api.Test;
+
+class LogisticsProjectCompilationTest extends CompilerE2ETestSupport {
+
+    @Test
+    void shouldCompileLogisticsBlueprint() {
+        CompilerResult result = compile("warehouse");
+        CompilationAssertions.succeeded(result.success());
+    }
+}
