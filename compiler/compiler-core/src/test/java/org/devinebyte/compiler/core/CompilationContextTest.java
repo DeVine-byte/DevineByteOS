095d898cc0a3bff0645f73c8905233a77f0ccda0
DeVine-byte <Divine4529@gmail.com>
2026-07-01 14:38:08 +0100

    Refactor CompilationContextTest to DefaultCompilerContextTest

--- compiler/compiler-core/src/test/java/org/devinebyte/compiler/core/CompilationContextTest.java
+++ compiler/compiler-core/src/test/java/org/devinebyte/compiler/core/CompilationContextTest.java
@@ -2,18 +2,20 @@ package org.devinebyte.compiler.core;
 
 import org.devinebyte.compiler.testing.support.CompilerTestSupport;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.nio.file.Path;
+import java.util.Map;
 
-class CompilationContextTest extends CompilerTestSupport {
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultCompilerContextTest extends CompilerTestSupport {
 
     @Test
-    void shouldCreateCompilationContext() {
-
-        CompilationContext context = new CompilationContext();
-
-        assertNotNull(context);
-
+    void shouldCreateCompilerContext(@TempDir Path tempDir) {
+        var ctx = new DefaultCompilerContext(tempDir, Map.of(), new DiagnosticCollector());
+        assertNotNull(ctx);
+        assertEquals(tempDir, ctx.workingDirectory());
+        assertNotNull(ctx.diagnostics());
     }
-
 }
