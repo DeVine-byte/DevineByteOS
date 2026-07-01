2fbedb4f533eab2dde9385a2abe6ab5950ec93f0
DeVine-byte <Divine4529@gmail.com>
2026-07-01 18:28:51 +0100

    Add MultiModuleCompilationTest for multi-module project

---
+++ compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/MultiModuleCompilationTest.java
@@ -0,0 +1,13 @@
+package org.devinebyte.compiler.e2e;
+
+import org.devinebyte.compiler.testing.assertions.CompilationAssertions;
+import org.junit.jupiter.api.Test;
+
+class MultiModuleCompilationTest extends CompilerE2ETestSupport {
+
+    @Test
+    void shouldCompileMultiModuleProject() {
+        CompilerResult result = compile("enterprise");
+        CompilationAssertions.succeeded(result.success());
+    }
+}
