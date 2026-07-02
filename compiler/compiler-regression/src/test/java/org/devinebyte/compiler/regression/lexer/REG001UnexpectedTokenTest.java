11a1bc3d4dd2b6b8f7480d65d0eba5fce6266a92
DeVine-byte <Divine4529@gmail.com>
2026-07-02 10:54:36 +0100

    Add REG001UnexpectedTokenTest for unexpected token handling

---
+++ compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/lexer/REG001UnexpectedTokenTest.java
@@ -0,0 +1,19 @@
+package org.devinebyte.compiler.regression.lexer;
+
+import org.devinebyte.compiler.regression.CompilerRegressionTestSupport;
+import org.devinebyte.compiler.regression.RegressionFixtures;
+import org.junit.jupiter.api.Test;
+
+import static org.devinebyte.compiler.testing.assertions.CompilationAssertions.succeeded;
+
+class REG001UnexpectedTokenTest extends CompilerRegressionTestSupport {
+
+    @Test
+    void shouldHandleUnexpectedTokenWithoutFailure() {
+        var result = compile(
+            RegressionFixtures.project("REG-001"),
+            RegressionFixtures.outputDirectory()
+        );
+        succeeded(result.success());
+    }
+}
