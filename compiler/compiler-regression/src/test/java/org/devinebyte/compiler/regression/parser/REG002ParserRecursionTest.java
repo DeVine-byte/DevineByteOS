bb2854aa2e407aefe0448350f16f5e5392555aee
DeVine-byte <Divine4529@gmail.com>
2026-07-02 10:55:25 +0100

    Add REG002ParserRecursionTest to prevent infinite recursion

---
+++ compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/parser/REG002ParserRecursionTest.java
@@ -0,0 +1,19 @@
+package org.devinebyte.compiler.regression.parser;
+
+import org.devinebyte.compiler.regression.CompilerRegressionTestSupport;
+import org.devinebyte.compiler.regression.RegressionFixtures;
+import org.junit.jupiter.api.Test;
+
+import static org.devinebyte.compiler.testing.assertions.CompilationAssertions.succeeded;
+
+class REG002ParserRecursionTest extends CompilerRegressionTestSupport {
+
+    @Test
+    void shouldNotEnterInfiniteRecursion() {
+        var result = compile(
+            RegressionFixtures.project("REG-002"),
+            RegressionFixtures.outputDirectory()
+        );
+        succeeded(result.success());
+    }
+}
