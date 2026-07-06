261e60203dfabd2b9df5c9388c1c554bf0513370
DeVine-byte <Divine4529@gmail.com>
2026-07-06 07:10:51 +0100

    Add Gradle configuration for compiler performance tests

---
+++ compiler/compiler-performance/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { java }
+
+dependencies {
+    implementation(project(":compiler-sdk"))
+    testImplementation(project(":compiler-testing"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    systemProperty "junit.jupiter.execution.parallel.enabled", "false" // benchmarks should be serial
+}
