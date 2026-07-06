c5282d8f29faa9f5f9b346b8f78645a3e642dc5a
DeVine-byte <Divine4529@gmail.com>
2026-07-06 07:16:13 +0100

    Add BenchmarkResult record for performance testing

---
+++ compiler/compiler-performance/src/test/java/org/devinebyte/compiler/performance/BenchmarkResult.java
@@ -0,0 +1,10 @@
+package org.devinebyte.compiler.performance;
+
+import org.devinebyte.sdk.CompilerResult;
+import java.time.Duration;
+
+public record BenchmarkResult(
+        CompilerResult result,
+        Duration duration
+) {
+}
