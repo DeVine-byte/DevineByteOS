255bfcb0e96866a4d1bcec76a8f468b31e1577eb
DeVine-byte <Divine4529@gmail.com>
2026-07-06 07:34:12 +0100

    Add performance benchmark test for code generation

---
+++ compiler/compiler-performance/src/test/java/org/devinebyte/compiler/performance/GeneratorBenchmarkTest.java
@@ -0,0 +1,13 @@
+package org.devinebyte.compiler.performance;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GeneratorBenchmarkTest extends CompilerBenchmarkSupport {
+
+    @Test
+    void shouldMeasureCodeGenerationPerformance() {
+        BenchmarkResult r = benchmark(BenchmarkFixtures.largeProject(), BenchmarkFixtures.outputDirectory());
+        assertTrue(r.result().success());
+    }
+}
