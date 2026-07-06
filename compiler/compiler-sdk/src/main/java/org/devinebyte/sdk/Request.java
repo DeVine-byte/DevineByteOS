ff28549ef5075a79cfbc90f4c83c7fbf615411a5
DeVine-byte <Divine4529@gmail.com>
2026-07-06 13:23:20 +0100

    Refactor Request class to use File instead of Path

--- compiler/compiler-sdk/src/main/java/org/devinebyte/sdk/Request.java
+++ compiler/compiler-sdk/src/main/java/org/devinebyte/sdk/Request.java
@@ -1,17 +1,24 @@
 package org.devinebyte.sdk;
 
-import java.nio.file.Path;
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 public final class Request {
+    private final File sourceFile;
+    private final File outputDirectory;
+    private final CompilerContext context;
+    private final boolean incremental;
 
-    private final Path sourceFile;
-
-    public Request(Path sourceFile) {
+    public Request(File sourceFile, File outputDirectory, CompilerContext context, boolean incremental) {
         this.sourceFile = sourceFile;
+        this.outputDirectory = outputDirectory;
+        this.context = context;
+        this.incremental = incremental;
     }
 
-    public Path getSourceFile() {
-        return sourceFile;
-    }
-
+    public File getSourceFile() { return sourceFile; }
+    public File getOutputDirectory() { return outputDirectory; }
+    public CompilerContext getContext() { return context; }
+    public boolean isIncremental() { return incremental; }
 }
