e248286ab3e410485bb709e57c9a0afc90906c78
DeVine-byte <Divine4529@gmail.com>
2026-07-06 08:54:45 +0100

    Update compile method to include CompilerContext

--- compiler/compiler-blueprint/src/main/java/org/devinebyte/compiler/blueprint/compiler/BlueprintCompiler.java
+++ compiler/compiler-blueprint/src/main/java/org/devinebyte/compiler/blueprint/compiler/BlueprintCompiler.java
@@ -1,9 +1,8 @@
 package org.devinebyte.compiler.blueprint.compiler;
 
 import org.devinebyte.compiler.parser.ast.ProgramNode;
+import org.devinebyte.sdk.CompilerContext;
 
 public interface BlueprintCompiler {
-
-    BlueprintCompilationResult compile(ProgramNode program);
-
+    BlueprintCompilationResult compile(ProgramNode program, CompilerContext context);
 }
