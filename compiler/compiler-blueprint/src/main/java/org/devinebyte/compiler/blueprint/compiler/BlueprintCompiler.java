725dd0d3934376096e539200ad810a2169f193c7
DeVine-byte <Divine4529@gmail.com>
2026-07-06 08:59:15 +0100

    Add import for BlueprintValidator in BlueprintCompiler

--- compiler/compiler-blueprint/src/main/java/org/devinebyte/compiler/blueprint/compiler/BlueprintCompiler.java
+++ compiler/compiler-blueprint/src/main/java/org/devinebyte/compiler/blueprint/compiler/BlueprintCompiler.java
@@ -1,5 +1,6 @@
 package org.devinebyte.compiler.blueprint.compiler;
 
+import org.devinebyte.compiler.blueprint.validation.BlueprintValidator;
 import org.devinebyte.compiler.parser.ast.ProgramNode;
 import org.devinebyte.sdk.CompilerContext;
 
