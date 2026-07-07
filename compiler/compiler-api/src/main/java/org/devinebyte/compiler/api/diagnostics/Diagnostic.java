8c2ee8b8817780cc0c660dc552426f61b5c3b458
DeVine-byte <Divine4529@gmail.com>
2026-07-07 08:17:07 +0000

    api package

--- compiler/compiler-api/src/main/java/org/devinebyte/compiler/api/diagnostics/Diagnostic.java
+++ compiler/compiler-api/src/main/java/org/devinebyte/compiler/api/diagnostics/Diagnostic.java
@@ -1,2 +1,2 @@
-package org.devinebyte.compiler.api;
+package org.devinebyte.compiler.api.diagnostics;
 public record Diagnostic(DiagnosticSeverity severity, String code, String message, int line, int column) {}
