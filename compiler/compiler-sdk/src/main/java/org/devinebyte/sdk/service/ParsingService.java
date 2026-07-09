427877973f380d9071f4a3080a1e3ea2c908f5f6
DeVine-byte <Divine4529@gmail.com>
2026-07-09 11:56:30 +0100

    Refactor ParsingService to use sdk types
    Updated imports to use sdk.Request and sdk.Result.

--- compiler/compiler-sdk/src/main/java/org/devinebyte/sdk/service/ParsingService.java
+++ compiler/compiler-sdk/src/main/java/org/devinebyte/sdk/service/ParsingService.java
@@ -1,11 +1,19 @@
 package org.devinebyte.sdk.service;
-import org.devinebyte.compiler.api.DiagnosticSeverity;
 
-import org.devinebyte.compiler.api.Request;
-import org.devinebyte.compiler.api.Result;
+import org.devinebyte.sdk.Request;
+import org.devinebyte.sdk.Result;
 
+/**
+ * Service responsible for lexical analysis and parsing.
+ */
 public interface ParsingService {
 
+    /**
+     * Parses the supplied request.
+     *
+     * @param request compilation request
+     * @return parsing result
+     */
     Result parse(Request request);
 
 }
