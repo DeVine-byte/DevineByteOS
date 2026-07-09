f676416e17133a3a866a863f6170204597be7eaa
DeVine-byte <Divine4529@gmail.com>
2026-07-09 22:20:11 +0100

    Fix import statement for Diagnostic class

--- compiler/compiler-sdk/src/main/java/org/devinebyte/sdk/Builder.java
+++ compiler/compiler-sdk/src/main/java/org/devinebyte/sdk/Builder.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.devinebyte.compiler.api.Diagnostic;
+import org.devinebyte.compiler.api.diagnostics.Diagnostic;
 
 public final class Builder {
 
