c20ce1bcf0ac0398c22bc62356774f5d02c15106
DeVine-byte <Divine4529@gmail.com>
2026-07-09 13:20:58 +0100

    Refactor RequestMapper class and compileRequest method
    Refactor RequestMapper to use builder pattern for Request creation.

--- compiler/compiler-cli/src/main/java/org/devinebyte/compiler/cli/sdk/RequestMapper.java
+++ compiler/compiler-cli/src/main/java/org/devinebyte/compiler/cli/sdk/RequestMapper.java
@@ -1,23 +1,14 @@
 package org.devinebyte.compiler.cli.sdk;
 
-import java.io.File;
-
-import org.devinebyte.compiler.cli.options.CliOptions;
 import org.devinebyte.sdk.Request;
 import org.devinebyte.sdk.Session;
+import org.devinebyte.compiler.cli.options.CliOptions;
 
-public final class RequestMapper {
-
+public class RequestMapper {
+    
     public Request compileRequest(Session session, CliOptions options) {
-
-        File sourceFile = options.getSourceFile().toFile();
-        File outputDirectory = session.getOutputDirectory().toFile();
-
-        return new Request(
-                sourceFile,
-                outputDirectory,
-                session,
-                options.isIncremental()
-        );
+        return Request.builder()
+                .source(options.getInput()) // was getSourceFile()
+                .build();
     }
 }
