e796b3f04af23eb90d51e4634e20df293c366e1c
DeVine-byte <Divine4529@gmail.com>
2026-07-09 13:00:26 +0100

    Refactor main method to instantiate CliApplication

--- compiler/compiler-cli/src/main/java/org/devinebyte/compiler/cli/Main.java
+++ compiler/compiler-cli/src/main/java/org/devinebyte/compiler/cli/Main.java
@@ -1,15 +1,15 @@
 package org.devinebyte.compiler.cli;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
 public final class Main {
 
     private Main() {
-        throw new AssertionError("Utility class");
     }
 
     public static void main(String[] args) {
-        int exitCode = CliApplication.run(args);
+
+        CliApplication application = new CliApplication();
+        int exitCode = application.run(args);
+
         System.exit(exitCode);
     }
-
 }
