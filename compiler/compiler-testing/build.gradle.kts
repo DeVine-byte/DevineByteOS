595909636f3a6139860884c0b21b755a00c2a732
DeVine-byte <Divine4529@gmail.com>
2026-07-10 08:04:40 +0100

    Change dependencies from implementation to api

--- compiler/compiler-testing/build.gradle.kts
+++ compiler/compiler-testing/build.gradle.kts
@@ -9,15 +9,17 @@ java {
 }
 
 dependencies {
-    implementation(project(":compiler-sdk")) 
-    implementation(project(":compiler-core"))
+
+    api(project(":compiler-sdk"))
+    api(project(":compiler-core"))
+
+    implementation("org.assertj:assertj-core:3.27.3")
+    implementation("org.mockito:mockito-core:5.18.0")
+    implementation("org.mockito:mockito-junit-jupiter:5.18.0")
+    implementation("com.google.jimfs:jimfs:1.3.0")
 
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.27.3")
-    testImplementation("org.mockito:mockito-core:5.18.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.18.0")
-    testImplementation("com.google.jimfs:jimfs:1.3.0")
 }
 
 tasks.test {
