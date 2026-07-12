3d44d87f071ad1605038c0009a3f1fcd6f9654cb
DeVine-byte <Divine4529@gmail.com>
2026-07-12 11:13:38 +0100

    Update InvalidProjectCompilationTest.java

--- compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/InvalidProjectCompilationTest.java
+++ compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/InvalidProjectCompilationTest.java
@@ -1,14 +1,21 @@
 package org.devinebyte.compiler.e2e;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
 import org.devinebyte.compiler.testing.assertions.CompilationAssertions;
+import org.devinebyte.compiler.testing.fixtures.FixtureManager;
+import org.devinebyte.sdk.Result;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
 
 class InvalidProjectCompilationTest extends CompilerE2ETestSupport {
 
     @Test
-    void shouldRejectInvalidProject() {
-        CompilerResult result = compile("invalid-project");
-        CompilationAssertions.failed(result.success());
+    void shouldFailToCompileInvalidProject() {
+
+        Path project = FixtureManager.project("invalid-project");
+
+        Result result = compile(project);
+
+        CompilationAssertions.assertFailed(result);
     }
 }
