26f0b032d5fdeda31cf5fbbb8e4632c0bbdfd003
DeVine-byte <Divine4529@gmail.com>
2026-07-12 11:11:43 +0100

    Refactor MultiModuleCompilationTest for enterprise project

--- compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/MultiModuleCompilationTest.java
+++ compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/MultiModuleCompilationTest.java
@@ -1,14 +1,21 @@
 package org.devinebyte.compiler.e2e;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
 import org.devinebyte.compiler.testing.assertions.CompilationAssertions;
+import org.devinebyte.compiler.testing.fixtures.FixtureManager;
+import org.devinebyte.sdk.Result;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
 
 class MultiModuleCompilationTest extends CompilerE2ETestSupport {
 
     @Test
-    void shouldCompileMultiModuleProject() {
-        CompilerResult result = compile("enterprise");
-        CompilationAssertions.succeeded(result.success());
+    void shouldCompileEnterpriseProject() {
+
+        Path project = FixtureManager.project("enterprise");
+
+        Result result = compile(project);
+
+        CompilationAssertions.assertSuccessful(result);
     }
 }
