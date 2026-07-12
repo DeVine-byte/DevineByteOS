06dddfe6bbab5583c7657cb9f7c0b2355df0f3b5
DeVine-byte <Divine4529@gmail.com>
2026-07-12 10:59:12 +0100

    Refactor test to compile Clinic project with fixtures

--- compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/ClinicProjectCompilationTest.java
+++ compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/ClinicProjectCompilationTest.java
@@ -1,14 +1,21 @@
 package org.devinebyte.compiler.e2e;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
 import org.devinebyte.compiler.testing.assertions.CompilationAssertions;
+import org.devinebyte.compiler.testing.fixtures.FixtureManager;
+import org.devinebyte.sdk.Result;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
 
 class ClinicProjectCompilationTest extends CompilerE2ETestSupport {
 
     @Test
-    void shouldCompileClinicBlueprint() {
-        CompilerResult result = compile("clinic");
-        CompilationAssertions.succeeded(result.success());
+    void shouldCompileClinicProject() {
+
+        Path project = FixtureManager.project("clinic");
+
+        Result result = compile(project);
+
+        CompilationAssertions.assertSuccessful(result);
     }
 }
