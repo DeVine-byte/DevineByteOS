3683c63f5823c0fe23a721308bff00a9c4f6d466
DeVine-byte <Divine4529@gmail.com>
2026-07-12 11:19:54 +0100

    Refactor DiagnosticsTest to check for invalid project

--- compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/DiagnosticsTest.java
+++ compiler/compiler-e2e/src/test/java/org/devinebyte/compiler/e2e/DiagnosticsTest.java
@@ -1,14 +1,21 @@
 package org.devinebyte.compiler.e2e;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
-import org.devinebyte.compiler.testing.assertions.DiagnosticAssertions;
+import org.devinebyte.compiler.testing.assertions.CompilationAssertions;
+import org.devinebyte.compiler.testing.fixtures.FixtureManager;
+import org.devinebyte.sdk.Result;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
 
 class DiagnosticsTest extends CompilerE2ETestSupport {
 
     @Test
-    void shouldProduceNoErrors() {
-        CompilerResult result = compile("school");
-        DiagnosticAssertions.hasNoErrors(result.diagnostics());
+    void shouldProduceDiagnosticsForInvalidProject() {
+
+        Path project = FixtureManager.project("school");
+
+        Result result = compile(project);
+
+        CompilationAssertions.assertDiagnosticCount(result, 0);
     }
 }
