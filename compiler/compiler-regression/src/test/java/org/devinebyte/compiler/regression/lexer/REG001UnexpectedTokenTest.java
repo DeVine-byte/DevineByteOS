05c7b4f868b850e0d28beb61ad0d78749a58af74
DeVine-byte <Divine4529@gmail.com>
2026-07-13 17:21:45 +0100

    Rename assertion method for clarity

--- compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/lexer/REG001UnexpectedTokenTest.java
+++ compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/lexer/REG001UnexpectedTokenTest.java
@@ -1,11 +1,11 @@
 package org.devinebyte.compiler.regression.lexer;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
+
 
 import org.devinebyte.compiler.regression.CompilerRegressionTestSupport;
 import org.devinebyte.compiler.regression.RegressionFixtures;
 import org.junit.jupiter.api.Test;
 
-import static org.devinebyte.compiler.testing.assertions.CompilationAssertions.succeeded;
+import static org.devinebyte.compiler.testing.assertions.CompilationAssertions.assertSuccessful;
 
 class REG001UnexpectedTokenTest extends CompilerRegressionTestSupport {
 
@@ -15,6 +15,6 @@ class REG001UnexpectedTokenTest extends CompilerRegressionTestSupport {
             RegressionFixtures.project("REG-001"),
             RegressionFixtures.outputDirectory()
         );
-        succeeded(result.success());
+        assertSuccessful(result.success());
     }
 }
