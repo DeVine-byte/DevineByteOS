0a6893bcff18eca862210ab39ef5235090ce7cac
DeVine-byte <Divine4529@gmail.com>
2026-07-13 16:26:53 +0100

    Format build.gradle.kts for better readability

--- compiler/compiler-performance/build.gradle.kts
+++ compiler/compiler-performance/build.gradle.kts
@@ -1,12 +1,16 @@
-plugins { java }
+plugins {
+    java
+}
 
 dependencies {
     implementation(project(":compiler-sdk"))
     testImplementation(project(":compiler-testing"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
 }
 
 tasks.test {
     useJUnitPlatform()
-    systemProperty ("junit.jupiter.execution.parallel.enabled", "false") // benchmarks should be serial
+    systemProperty(
+        "junit.jupiter.execution.parallel.enabled",
+        "false"
+    )
 }
