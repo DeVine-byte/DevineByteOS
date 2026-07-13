00bd5b8f21737a069af8959df732789416ccbaf6
DeVine-byte <Divine4529@gmail.com>
2026-07-13 09:54:34 +0100

    Refactor BlueprintBenchmarkTest to extend BenchmarkSupport

--- compiler/compiler-performance/src/test/java/org/devinebyte/compiler/performance/BlueprintBenchmarkTest.java
+++ compiler/compiler-performance/src/test/java/org/devinebyte/compiler/performance/BlueprintBenchmarkTest.java
@@ -1,14 +1,17 @@
 package org.devinebyte.compiler.performance;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class BlueprintBenchmarkTest extends CompilerBenchmarkSupport {
+class BlueprintBenchmarkTest extends BenchmarkSupport {
 
     @Test
     void shouldMeasureBlueprintCompilationPerformance() {
-        BenchmarkResult r = benchmark(BenchmarkFixtures.mediumProject(), BenchmarkFixtures.outputDirectory());
-        assertTrue(r.result().success());
+
+        BenchmarkResult result =
+                benchmark(BenchmarkFixtures.clinicProject());
+
+        assertTrue(result.result().successful());
     }
 }
