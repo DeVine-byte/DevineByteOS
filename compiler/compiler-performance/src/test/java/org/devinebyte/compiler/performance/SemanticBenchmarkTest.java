860b1890277cc9c74523af70ade9c3f297e4ea87
DeVine-byte <Divine4529@gmail.com>
2026-07-13 09:53:53 +0100

    Refactor SemanticBenchmarkTest to extend BenchmarkSupport

--- compiler/compiler-performance/src/test/java/org/devinebyte/compiler/performance/SemanticBenchmarkTest.java
+++ compiler/compiler-performance/src/test/java/org/devinebyte/compiler/performance/SemanticBenchmarkTest.java
@@ -1,14 +1,17 @@
 package org.devinebyte.compiler.performance;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class SemanticBenchmarkTest extends CompilerBenchmarkSupport {
+class SemanticBenchmarkTest extends BenchmarkSupport {
 
     @Test
     void shouldMeasureSemanticAnalysisPerformance() {
-        BenchmarkResult r = benchmark(BenchmarkFixtures.largeProject(), BenchmarkFixtures.outputDirectory());
-        assertTrue(r.result().success());
+
+        BenchmarkResult result =
+                benchmark(BenchmarkFixtures.schoolProject());
+
+        assertTrue(result.result().successful());
     }
 }
