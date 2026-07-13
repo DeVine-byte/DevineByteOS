cabb526d00e2294b8376644e2dad8355e8db21a1
DeVine-byte <Divine4529@gmail.com>
2026-07-13 17:20:23 +0100

    Update assertion method in REG002ParserRecursionTest

--- compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/parser/REG002ParserRecursionTest.java
+++ compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/parser/REG002ParserRecursionTest.java
@@ -1,11 +1,10 @@
 package org.devinebyte.compiler.regression.parser;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
 import org.devinebyte.compiler.regression.CompilerRegressionTestSupport;
 import org.devinebyte.compiler.regression.RegressionFixtures;
 import org.junit.jupiter.api.Test;
 
-import static org.devinebyte.compiler.testing.assertions.CompilationAssertions.succeeded;
+import static org.devinebyte.compiler.testing.assertions.CompilationAssertions.assertSuccessful;
 
 class REG002ParserRecursionTest extends CompilerRegressionTestSupport {
 
@@ -15,6 +14,6 @@ class REG002ParserRecursionTest extends CompilerRegressionTestSupport {
             RegressionFixtures.project("REG-002"),
             RegressionFixtures.outputDirectory()
         );
-        succeeded(result.success());
+        assertSuccessful(result.success());
     }
 }
