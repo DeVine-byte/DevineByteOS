15f7c0b6f57dea72882aa773df7ad1986aa77c1d
DeVine-byte <Divine4529@gmail.com>
2026-07-13 17:23:43 +0100

    Update assertion method in REG004BlueprintValidationTest

--- compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/blueprint/REG004BlueprintValidationTest.java
+++ compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/blueprint/REG004BlueprintValidationTest.java
@@ -1,11 +1,10 @@
 package org.devinebyte.compiler.regression.blueprint;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
 
 import org.devinebyte.compiler.regression.CompilerRegressionTestSupport;
 import org.devinebyte.compiler.regression.RegressionFixtures;
 import org.junit.jupiter.api.Test;
 
-import static org.devinebyte.compiler.testing.assertions.CompilationAssertions.succeeded;
+import static org.devinebyte.compiler.testing.assertions.CompilationAssertions.assertSuccessful;
 
 class REG004BlueprintValidationTest extends CompilerRegressionTestSupport {
 
@@ -15,6 +14,6 @@ class REG004BlueprintValidationTest extends CompilerRegressionTestSupport {
             RegressionFixtures.project("REG-004"),
             RegressionFixtures.outputDirectory()
         );
-        succeeded(result.success());
+        assertSuccessful(result.success());
     }
 }
