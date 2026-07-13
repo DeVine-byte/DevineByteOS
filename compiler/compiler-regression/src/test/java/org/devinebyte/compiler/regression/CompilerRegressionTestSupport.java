6241df6df9522d7087ce1d7ae70debb1208365dc
DeVine-byte <Divine4529@gmail.com>
2026-07-13 17:10:57 +0100

    Refactor compile method to use new SDK structure

--- compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/CompilerRegressionTestSupport.java
+++ compiler/compiler-regression/src/test/java/org/devinebyte/compiler/regression/CompilerRegressionTestSupport.java
@@ -1,22 +1,26 @@
 package org.devinebyte.compiler.regression;
-import org.devinebyte.compiler.api.diagnostics.DiagnosticSeverity;
-
-import org.devinebyte.compiler.api.CompilerContext;
-import org.devinebyte.compiler.api.CompilerRequest;
-import org.devinebyte.compiler.api.CompilerResult;
-import org.devinebyte.compiler.api.CompilerSDK;
-import org.devinebyte.compiler.api.Session;
 
 import java.nio.file.Path;
 
+import org.devinebyte.sdk.CompilerSDK;
+import org.devinebyte.sdk.Request;
+import org.devinebyte.sdk.Result;
+import org.devinebyte.sdk.Session;
+
 public abstract class CompilerRegressionTestSupport {
 
-    protected CompilerResult compile(Path project, Path output) {
-        CompilerSDK sdk = CompilerSDK.builder().build();
-        Session session = sdk.createSession();
-        CompilerContext ctx = sdk.createContext();
+    protected Result compile(Path project, Path output) {
 
-        CompilerRequest request = new CompilerRequest(project, output, ctx, false);
+        Session session = CompilerSDK.builder()
+                .projectRoot(project)
+                .sourceDirectory(project)
+                .outputDirectory(output)
+                .incremental(false)
+                .optimize(false)
+                .build();
+
+        Request request = session.request(project);
+
         return session.compile(request);
     }
 }
