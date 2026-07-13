1c3eabbfccc11074afe33753dda20bbbbd6c8ddd
DeVine-byte <Divine4529@gmail.com>
2026-07-13 13:12:56 +0100

    Fix assertion method name in benchmark test

--- compiler/compiler-performance/src/test/java/org/devinebyte/compiler/performance/MultiModuleBenchmarkTest.java
+++ compiler/compiler-performance/src/test/java/org/devinebyte/compiler/performance/MultiModuleBenchmarkTest.java
@@ -12,6 +12,6 @@ class MultiModuleBenchmarkTest extends BenchmarkSupport {
         BenchmarkResult result =
                 benchmark(BenchmarkFixtures.multiModuleProject());
 
-        assertTrue(result.result().successful());
+        assertTrue(result.result().success());
     }
 }
