8510d7247ced547eaa444c961982fe8ac07f8173
DeVine-byte <Divine4529@gmail.com>
2026-07-18 17:05:07 +0100

    Refactor DefaultParser to accept token list directly

--- compiler/compiler-parser/src/main/java/org/devinebyte/compiler/parser/parser/DefaultParser.java
+++ compiler/compiler-parser/src/main/java/org/devinebyte/compiler/parser/parser/DefaultParser.java
@@ -4,8 +4,6 @@ import org.devinebyte.compiler.parser.ast.EntityNode;
 import org.devinebyte.compiler.parser.ast.ProgramNode;
 import org.devinebyte.compiler.parser.ast.PropertyNode;
 import org.devinebyte.compiler.parser.ast.WorkflowNode;
-import org.devinebyte.compiler.parser.lexer.DefaultLexer;
-import org.devinebyte.compiler.parser.lexer.Lexer;
 import org.devinebyte.compiler.parser.lexer.Token;
 import org.devinebyte.compiler.parser.lexer.TokenType;
 
@@ -14,19 +12,17 @@ import java.util.List;
 /**
  * Default implementation of the DevineByte parser.
  *
- * Converts a stream of tokens into an Abstract Syntax Tree (AST).
+ * Converts a token stream into an Abstract Syntax Tree (AST).
  */
 public final class DefaultParser implements Parser {
-
-    private final Lexer lexer = new DefaultLexer();
 
     private List<Token> tokens;
     private int position;
 
     @Override
-    public ProgramNode parse(String source) {
+    public ProgramNode parse(List<Token> tokens) {
 
-        this.tokens = lexer.tokenize(source);
+        this.tokens = tokens;
         this.position = 0;
 
         ProgramNode program = new ProgramNode();
@@ -61,45 +57,55 @@ public final class DefaultParser implements Parser {
         EntityNode entity = new EntityNode();
 
         entity.setName(
-                consume(TokenType.IDENTIFIER,
-                        "Expected entity name.")
-                        .value()
+                consume(
+                        TokenType.IDENTIFIER,
+                        "Expected entity name."
+                ).value()
         );
 
-        consume(TokenType.LEFT_BRACE,
-                "Expected '{'.");
+        consume(
+                TokenType.LEFT_BRACE,
+                "Expected '{'."
+        );
 
         while (!check(TokenType.RIGHT_BRACE)) {
-
             entity.getProperties().add(parseProperty());
         }
 
-        consume(TokenType.RIGHT_BRACE,
-                "Expected '}'.");
+        consume(
+                TokenType.RIGHT_BRACE,
+                "Expected '}'."
+        );
 
         return entity;
     }
 
     private PropertyNode parseProperty() {
 
-        consume(TokenType.PROPERTY,
-                "Expected 'property'.");
+        consume(
+                TokenType.PROPERTY,
+                "Expected 'property'."
+        );
 
         PropertyNode property = new PropertyNode();
 
         property.setName(
-                consume(TokenType.IDENTIFIER,
-                        "Expected property name.")
-                        .value()
+                consume(
+                        TokenType.IDENTIFIER,
+                        "Expected property name."
+                ).value()
         );
 
-        consume(TokenType.COLON,
-                "Expected ':'.");
+        consume(
+                TokenType.COLON,
+                "Expected ':'."
+        );
 
         property.setType(
-                consume(TokenType.IDENTIFIER,
-                        "Expected property type.")
-                        .value()
+                consume(
+                        TokenType.IDENTIFIER,
+                        "Expected property type."
+                ).value()
         );
 
         if (check(TokenType.SEMICOLON)) {
@@ -111,31 +117,41 @@ public final class DefaultParser implements Parser {
 
     private WorkflowNode parseWorkflow() {
 
-        consume(TokenType.WORKFLOW,
-                "Expected 'workflow'.");
+        consume(
+                TokenType.WORKFLOW,
+                "Expected 'workflow'."
+        );
 
         WorkflowNode workflow = new WorkflowNode();
 
         workflow.setName(
-                consume(TokenType.IDENTIFIER,
-                        "Expected workflow name.")
-                        .value()
+                consume(
+                        TokenType.IDENTIFIER,
+                        "Expected workflow name."
+                ).value()
         );
 
-        consume(TokenType.LEFT_BRACE,
-                "Expected '{'.");
+        consume(
+                TokenType.LEFT_BRACE,
+                "Expected '{'."
+        );
 
         while (!check(TokenType.RIGHT_BRACE)) {
             advance();
         }
 
-        consume(TokenType.RIGHT_BRACE,
-                "Expected '}'.");
+        consume(
+                TokenType.RIGHT_BRACE,
+                "Expected '}'."
+        );
 
         return workflow;
     }
 
-    private Token consume(TokenType expected, String message) {
+    private Token consume(
+            TokenType expected,
+            String message
+    ) {
 
         if (!check(expected)) {
             throw error(message);
@@ -163,17 +179,14 @@ public final class DefaultParser implements Parser {
     }
 
     private boolean isAtEnd() {
-
         return peek().type() == TokenType.EOF;
     }
 
     private Token peek() {
-
         return tokens.get(position);
     }
 
     private Token previous() {
-
         return tokens.get(position - 1);
     }
 
